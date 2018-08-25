78dec6d404ae7ad58bc7c12326264f763992bd12
charlie <charliephung@gmail.com>
2018-08-25 17:58:07 +0300

    lazy import app

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "reducers";
 import ReactDOM from "react-dom";
+import Spinner from "components/common/loading/Spinner";
+
 import App from "App";
 import thunk from "redux-thunk";
 import { firebaseConfig } from "configs/firebase.js";
 import firebase from "firebase";
+import DynamicImport from "components/feature/DynamicImport";
 
 const store = createStore(
   rootReducer,
@@ -30,9 +33,14 @@ fireAuth.onAuthStateChanged(user => {
   }
 });
 
+const App = props => (
+  <DynamicImport
+    load={() => import("App")}
+    render={Comp => (Comp === null ? <Spinner /> : <Comp {...props} />)}
+  />
+);
+
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <Provider store={store}>{App}</Provider>,
   document.getElementById("root")
 );
